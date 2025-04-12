93913c4956177b209bcf429ebe68b8297c68bc37
anhnd <anhnd012@gmail.com>
2025-04-12 14:16:46 +0700

    Update code

--- src/main/java/com/example/leavingrecordsystemapplicationlogicdemo/repository/CreateEmployee.java
+++ src/main/java/com/example/leavingrecordsystemapplicationlogicdemo/repository/CreateEmployee.java
@@ -13,26 +13,19 @@ public class CreateEmployee {
     private final DSLContext dsl;
 
     public EmployeeModel execute(EmployeeModel employeeModel) {
-        Integer empId = dsl.insertInto(EMPLOYEE)
+        return dsl.insertInto(EMPLOYEE)
                 .set(EMPLOYEE.NAME, employeeModel.getName())
                 .set(EMPLOYEE.EMAIL, employeeModel.getEmail())
                 .set(EMPLOYEE.MOBILE, employeeModel.getMobile())
                 .set(EMPLOYEE.E_POSITION, employeeModel.getPosition())
                 .set(EMPLOYEE.STATUS, employeeModel.getStatus())
                 .set(EMPLOYEE.MANAGER_ID, employeeModel.getManagerId())
-                .returning(EMPLOYEE.ID)
-                .fetchOne()
-                .get(EMPLOYEE.ID);
-
-        return dsl.select(EMPLOYEE.NAME, EMPLOYEE.EMAIL, EMPLOYEE.MOBILE, EMPLOYEE.STATUS, EMPLOYEE.E_POSITION, EMPLOYEE.MANAGER_ID)
-                .from(EMPLOYEE)
-                .where(EMPLOYEE.ID.eq(empId))
-                .fetchOne(record -> new EmployeeModel(
-                        record.get(EMPLOYEE.NAME),
-                        record.get(EMPLOYEE.EMAIL),
-                        record.get(EMPLOYEE.MOBILE),
-                        record.get(EMPLOYEE.E_POSITION),
-                        record.get(EMPLOYEE.STATUS)
-                ));
+                .returningResult(EMPLOYEE.NAME,
+                        EMPLOYEE.EMAIL,
+                        EMPLOYEE.MOBILE,
+                        EMPLOYEE.E_POSITION,
+                        EMPLOYEE.STATUS,
+                        EMPLOYEE.MANAGER_ID)
+                .fetchOneInto(EmployeeModel.class);
     }
 }
